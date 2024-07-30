9f38a028e5cedb13daad42134e999c85cbbb9270
Datdev28 <nguyenquangdat2842003@gmail.com>
2024-07-30 17:33:17 +0700

    up load source code

---
+++ coffee/js/main.js
@@ -0,0 +1,20 @@
+  let btn_change_background;
+  let dot_change;
+  btn_change_background = document.querySelector('.change-background');
+  dot_change = btn_change_background.querySelectorAll('.dot');
+  dot_change.forEach((value) => {
+    value.addEventListener('click', ()=> {
+      const link_img = value.dataset.linkImg;
+      document.querySelector('.section-1').style.backgroundImage = `url(${link_img})`;
+       if(!value.classList.contains('is-active')){
+        check();
+        value.classList.add('is-active');
+       }
+    });
+  });
+  function check (){
+    const check_is = btn_change_background.querySelector('.is-active');
+    if(check_is){
+      check_is.classList.remove('is-active');
+    }
+  }
